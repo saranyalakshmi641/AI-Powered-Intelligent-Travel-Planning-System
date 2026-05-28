00e3303fbe64ad8463f7243419ec84ccdfd0f962
Vendra Lakshmi Saranya <saranyalakshmi641@gmail.com>
2026-05-28 08:02:49 +0530

    Update llm.py

--- utils/llm.py
+++ utils/llm.py
@@ -1,8 +1,36 @@
-from langchain_groq import ChatGroq
+'''from langchain_groq import ChatGroq
 from config import GROQ_API_KEY, MODEL_NAME
 
 llm = ChatGroq(
     groq_api_key=GROQ_API_KEY,
+    model_name=MODEL_NAME,
+    temperature=0.7
+)
+
+def ask_ai(prompt):
+
+    try:
+
+        response = llm.invoke(prompt)
+
+        return response.content
+
+    except Exception as e:
+
+        return f"AI Error: {str(e)}"
+'''
+
+import os
+
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+from config import MODEL_NAME
+
+load_dotenv()
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
     model_name=MODEL_NAME,
     temperature=0.7
 )
